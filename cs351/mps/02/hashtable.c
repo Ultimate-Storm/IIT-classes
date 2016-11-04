2e4045f103c3ea0266a5667f09041e99180bc413
Flamacue <asumner@hawk.iit.edu>
2016-11-04 01:40:01 -0400

    finish mp02

--- cs351/mps/02/hashtable.c
+++ cs351/mps/02/hashtable.c
@@ -24,11 +24,26 @@ hashtable_t *make_hashtable(unsigned long size) {
 void ht_put(hashtable_t *ht, char *key, void *val) {
   /* FIXME: the current implementation doesn't update existing entries */
   unsigned int idx = hash(key) % ht->size;
-  bucket_t *b = malloc(sizeof(bucket_t));
-  b->key = key;
-  b->val = val;
-  b->next = ht->buckets[idx];
-  ht->buckets[idx] = b;
+  int key_found = 0;
+  bucket_t *it = ht->buckets[idx];
+  while (it) {
+    if (!strcmp(it->key, key)){
+      key_found = 1;
+      free(it->key);
+      free(it->val);
+      it->key = key;
+      it->val = val;
+      break;
+    }
+    it = it->next;
+  }
+  if (!key_found){
+    bucket_t *b = malloc(sizeof(bucket_t));
+    b->key = key;
+    b->val = val;
+    b->next = ht->buckets[idx];
+    ht->buckets[idx] = b;
+  }
 }
 
 void *ht_get(hashtable_t *ht, char *key) {
@@ -58,12 +73,88 @@ void ht_iter(hashtable_t *ht, int (*f)(char *, void *)) {
 }
 
 void free_hashtable(hashtable_t *ht) {
+  bucket_t *b;
+  unsigned long i;
+  for (i=0; i<ht->size; i++) {
+    b = ht->buckets[i];
+    while (b) {
+      if (b->next){
+        bucket_t *temp = b;
+        b = b->next;
+        free(temp->key);
+        free(temp->val);
+        free(temp);
+        temp = NULL;
+      } else {
+          free(b->key);
+          free(b->val);
+          free(b);
+          b = NULL;
+          break;
+      }
+    }
+  }
+  free(ht->buckets);
   free(ht); // FIXME: must free all substructures!
+  ht = NULL;
 }
 
 /* TODO */
 void  ht_del(hashtable_t *ht, char *key) {
+  unsigned int idx = hash(key) % ht->size;
+  bucket_t *b = ht->buckets[idx];
+  bucket_t *next = b->next;
+  if(!strcmp(b->key, key)){
+    ht->buckets[idx] = b->next;
+    free(b->key);
+    free(b->val);
+    free(b);
+    b = NULL;
+    return;
+  }
+  while (next){
+    if (!strcmp(next->key, key)){
+      free(next->key);
+      free(next->val);
+      b->next = next->next;
+      free(next);
+      next = NULL;
+      return;
+    }
+    b = b->next;
+    next = next->next;
+  }
 }
 
 void  ht_rehash(hashtable_t *ht, unsigned long newsize) {
+  hashtable_t *nht = make_hashtable(newsize);
+  unsigned long i;
+  for (i=0; i<ht->size; i++){
+    bucket_t * b = ht->buckets[i];
+    while (b){
+        char *nkey = malloc(strlen(b->key)+1);
+        void *nval = malloc(strlen(b->val)+1);
+        strcpy(nkey, b->key);
+        strcpy(nval, b->val);
+        ht_put(nht, nkey, nval);
+      if (b->next){
+        bucket_t *temp = b;
+        b = b->next;
+        free(temp->key);
+        free(temp->val);
+        free(temp);
+      } else {
+        free(b->key);
+        free(b->val);
+        free(b);
+        b = NULL;
+        break;
+      }
+    }
+  }
+  free(ht->buckets);
+  ht->buckets = nht->buckets;
+  ht->size = nht->size;
+  free(nht);
+  nht = NULL;
 }
