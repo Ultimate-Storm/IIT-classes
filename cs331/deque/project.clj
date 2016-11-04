ef6bb559860a8e5dda00d6c0ea7d1b09c03a7c52
Adam K. Sumner <asumner@hawk.iit.edu>
2016-11-03 22:57:04 -0400

    readd cs331

---
+++ cs331/deque/project.clj
@@ -0,0 +1,6 @@
+(defproject deque "0.0.1-SNAPSHOT"
+  :description "A doubly ended queue for CS 331."
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles {:dev {:dependencies [[midje "1.5.0"]]}}
+  :main deque.core)
+  
